2668f3ac773f1401ae6b08a318834909924ab427
Leevi Seppäläinen <AA3567@student.jamk.fi>
2020-09-10 09:28:47 +0300

    Added code to print input

--- examples/hellopython/hellopython.py
+++ examples/hellopython/hellopython.py
@@ -1,11 +1,9 @@
-# import a datetime libray to access current date and time
 import datetime
-
-# declare a variable named 'msg' and set some text into it
 message = "Tämän ohjelman suoritti Leevi Seppäläinen"
-
-# print the message into console
 print(message)
-
-# print the current date and time
 print(datetime.datetime.now())
+
+# read text from console and echo back
+text = input ("Enter some text: ")
+print("you wrote: " + text)
+
