c5cf2e5ecace1fba8640e22cec97717b7805526a
Leevi Seppäläinen <AA3567@student.jamk.fi>
2020-09-10 09:22:29 +0300

    Added hello python to project

---
+++ examples/hellopython/hellopython.py
@@ -0,0 +1,11 @@
+# import a datetime libray to access current date and time
+import datetime
+
+# declare a variable named 'msg' and set some text into it
+message = "Tämän ohjelman suoritti Leevi Seppäläinen"
+
+# print the message into console
+print(message)
+
+# print the current date and time
+print(datetime.datetime.now())
